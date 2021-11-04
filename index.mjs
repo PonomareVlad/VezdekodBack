36bea3a22237861be1ff6ddc7d54a8bc24661a16
PonomareVlad <Git@PonomareVlad.ru>
2021-11-05 03:43:48 +0500

    Vercel image uploading test

--- index.mjs
+++ index.mjs
@@ -1,7 +1,22 @@
-export default async (req, res) => res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-    headers: req.headers,
-    versions: process.versions
+import Router from 'router'
+import formidable from 'formidable'
+import finalhandler from 'finalhandler'
+import {readFile} from "fs/promises";
+
+const router = Router().get('/get', async (req, res) => {
+    return await res.send('Get')
+}).post('/upload', async (req, res) => {
+    const form = formidable({});
+    const {fields, files} = await new Promise((resolve, reject) => {
+        form.parse(req, (err, fields, files) => {
+            if (err) return reject(err);
+            console.debug({fields, files});
+            resolve({fields, files});
+        });
+    });
+    if (!files || !files.image) return await res.send('No image');
+    res.setHeader('Content-Type', files.image.mimetype);
+    return await res.send(await readFile(files.image.filepath))
 })
+
+export default async (req, res) => await router(req, res, finalhandler(req, res))
