36020fc3cd5a1f1453ee295d5bbf53c3801bdd0b
PonomareVlad <Git@PonomareVlad.ru>
2021-11-05 05:44:55 +0500

    MySQL image uploading test

--- index.mjs
+++ index.mjs
@@ -1,10 +1,25 @@
 import Router from 'router'
 import formidable from 'formidable'
 import finalhandler from 'finalhandler'
-import {readFile} from "fs/promises";
+import {readFile} from "fs/promises"
+import mysql from 'mysql2/promise'
+
+const mysqlCredentials = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+}
 
 const router = Router().get('/get', async (req, res) => {
-    return await res.send('Get')
+    if (!req.query.id) return res.send('No parameter')
+    const connection = await mysql.createConnection(mysqlCredentials);
+    const [rows, fields] = await connection.execute('SELECT * FROM `images` WHERE id=?', [parseInt(req.query.id)]);
+    console.debug({rows});
+    if (!rows || !rows.length) return res.send('Not found')
+    const target = rows.shift();
+    res.setHeader('Content-Type', target.mimetype);
+    return await res.send(target.image)
 }).post('/upload', async (req, res) => {
     const form = formidable({});
     const {fields, files} = await new Promise((resolve, reject) => {
@@ -15,8 +30,11 @@ const router = Router().get('/get', async (req, res) => {
         });
     });
     if (!files || !files.image) return await res.send('No image');
-    res.setHeader('Content-Type', files.image.mimetype);
-    return await res.send(await readFile(files.image.filepath))
+    const connection = await mysql.createConnection(mysqlCredentials);
+    const image = await readFile(files.image.filepath);
+    const result = await connection.execute('INSERT INTO `images` SET image=?, mimetype=?', [image, files.image.mimetype]);
+    console.debug(result);
+    return await res.json({id: result.shift().insertId})
 })
 
 export default async (req, res) => await router(req, res, finalhandler(req, res))
